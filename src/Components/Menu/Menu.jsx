f2606ef2c9b1190e9633af3345ed224addb8d0df
SuhrobTeshaev <125867321+SuhrobTeshaev@users.noreply.github.com>
2023-11-07 05:57:38 +0700

    multiLevelList

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,87 @@
+import './Menu.css'
+import {NestedList} from "../NestedList/NestedList";
+
+const menuItems = [
+    {
+        label: "Меню 1",
+        items: [
+            {
+                label: "Меню 1.1",
+                items: [],
+            },
+            {
+                label: "Меню 1.2",
+                items: [
+                    {
+                        label: "Подпункт 1.2.1",
+                        items: [],
+                    },
+                    {
+                        label: "Подпункт 1.2.2",
+                        items: [],
+                    },
+                    {
+                        label: "Подпункт 1.2.3",
+                        items: [],
+                    },
+                ],
+            },
+            {
+                label: "Меню 1.3",
+                items: [],
+            },
+            {
+                label: "Меню 1.4",
+                items: [],
+            },
+        ],
+    },
+    {
+        label: "Меню 2",
+        items: [
+            {
+                label: "Подпункт 2.1",
+                items: [],
+            },
+            {
+                label: "Подпункт 2.2",
+                items: [],
+            },
+            {
+                label: "Подпункт 2.3",
+                items: [],
+            },
+        ],
+    },
+    {
+        label: "Меню 3",
+        items: [
+            {
+                label: "Подпункт 3.1",
+                items: [],
+            },
+        ],
+    },
+    {
+        label: "Меню 4",
+        items: [],
+    },
+];
+export const Menu = () => {
+    const handleShowJSON = () => {
+        console.log(JSON.stringify({ root: menuItems }));
+    };
+
+    return (
+        <div className="menu">
+            <div className="menu__header">
+                <h2 className='menu__title'>Меню</h2>
+            </div>
+            <div className="menu__content">
+                {menuItems.length > 0 && <NestedList list={menuItems} />}
+
+                <button className='menu__json-btn' onClick={handleShowJSON}>Show JSON</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/NestedList/NestedList.jsx
@@ -0,0 +1,23 @@
+import './NestedList.css'
+export const NestedList = ({ list }) => {
+    const toggleChildren = (e) => {
+        const hasClass = e.target.classList.contains('isActive')
+        if(hasClass) {
+            e.target.classList.remove('isActive')
+        } else {
+            e.target.classList.add('isActive')
+        }
+    };
+    return (
+        <ul>
+            {list.map(({ label, items }, index) => (
+                <li key={index}>
+                    <p className={!!items.length ? 'hasChildren' : ''} onClick={(event) => items.length > 0 && toggleChildren(event)}>{label}</p>
+                    {!!items.length && (
+                        <NestedList list={items} />
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
+};
